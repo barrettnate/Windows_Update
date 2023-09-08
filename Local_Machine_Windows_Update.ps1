68d62561e15b9829bb59891bec14624ef5d02cce
barrettnate <nate_barrett@hillspet.com>
2023-09-08 11:01:29 -0500

    Initial Upload

---
+++ Local_Machine_Windows_Update.ps1
@@ -0,0 +1,41 @@
+﻿Function Check-RunAsAdministrator()
+{
+  #Get current user context
+  $CurrentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+  
+  #Check user is running the script is member of Administrator Group
+  if($CurrentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator))
+  {
+       Write-host "Script is running with Administrator privileges!"
+  }
+  else
+    {
+       #Create a new Elevated process to Start PowerShell
+       $ElevatedProcess = New-Object System.Diagnostics.ProcessStartInfo "PowerShell";
+ 
+       # Specify the current script path and name as a parameter
+       $ElevatedProcess.Arguments = "& '" + $script:MyInvocation.MyCommand.Path + "'"
+ 
+       #Set the Process to elevated
+       $ElevatedProcess.Verb = "runas"
+ 
+       #Start the new elevated process
+       [System.Diagnostics.Process]::Start($ElevatedProcess)
+ 
+       #Exit from the current, unelevated, process
+       Exit
+ 
+    }
+}
+ 
+#Check Script is running with Elevated Privileges
+Check-RunAsAdministrator
+ 
+# Check if the PSWindowsUpdate module is installed
+if (!(Get-Module -Name PSWindowsUpdate)) {
+    # If the module is not installed, install it using the Install-Module cmdlet
+    Install-Module PSWindowsUpdate -Force }
+    
+#Place your script here.
+Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -AutoReboot -ForceInstall
+Start-Sleep -Seconds 30
